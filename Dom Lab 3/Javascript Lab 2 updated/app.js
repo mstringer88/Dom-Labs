b37a92ce6b84fa8904d378be1e6d4acec27d4ec1
Michael Stringer <mstringer09@yahoo.com>
2018-08-22 15:24:56 -0500

    Changes to Lab 3

--- Dom Lab 3/Javascript Lab 2 updated/app.js
+++ Dom Lab 3/Javascript Lab 2 updated/app.js
@@ -10,109 +10,107 @@ document.addEventListener("DOMContentLoaded", function () {
 
     button.addEventListener("click", function () {
 
-        let friends = ["Matt", "Gio", "Logan", "Nate", "Mike"];
+        // let friends = ["Matt", "Gio", "Logan", "Nate", "Mike"];
 
-        for (let i = 0; i < friends.length; i++) {
 
-            for (let j = 99; j > 0; j--) {
+        // let Matt = document.createElement("div");
+        // let Gio = document.createElement("div");
+        // let Logan = document.createElement("div");
+        // let Nate = document.createElement("div");
+        // let Mike = document.createElement("div");
 
-                if (j >= 3) {
-                    console.log (j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out," + [j - 1] + " lines of code in the file.");
-                }
-                else if (j >= 2) {
-                    console.log (j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out," + [j - 1] + " line of the code in the file.");
-                }
-                else {
-                    console.log (j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file.");
-                };
-            }
-        };
+        // Matt.className = "friends";
+        // Gio.className = "friends";
+        // Logan.className = "friends";
+        // Nate.className = "friends";
+        // Mike.className = "friends";
 
-        let Matt = document.createElement("div");
-        let Gio = document.createElement("div");
-        let Logan = document.createElement("div");
-        let Nate = document.createElement("div");
-        let Mike = document.createElement("div");
+        // document.body.appendChild(Matt);
 
-        Matt.className = "friends";
-        Gio.className = "friends";
-        Logan.className = "friends";
-        Nate.className = "friends";
-        Mike.className = "friends";
+        // let h3Matt = document.createElement("h3");
+        // let h3MattName = document.createTextNode("Matt");
+        // h3Matt.appendChild(h3MattName);
+        // Matt.appendChild(h3Matt);
 
-        document.body.appendChild(Matt);
+        // document.body.appendChild(Gio);
 
-        let h3Matt = document.createElement("h3");
-        let h3MattName = document.createTextNode("Matt");
-        h3Matt.appendChild(h3MattName);
-        Matt.appendChild(h3Matt);
+        // let h3Gio = document.createElement("h3");
+        // let h3GioName = document.createTextNode("Gio");
+        // h3Gio.appendChild(h3GioName);
+        // Gio.appendChild(h3Gio);
 
-        document.body.appendChild(Gio);
+        // document.body.appendChild(Logan);
 
-        let h3Gio = document.createElement("h3");
-        let h3GioName = document.createTextNode("Gio");
-        h3Gio.appendChild(h3GioName);
-        Gio.appendChild(h3Gio);
+        // let h3Logan = document.createElement("h3");
+        // let h3LoganName = document.createTextNode("Logan");
+        // h3Logan.appendChild(h3LoganName);
+        // Logan.appendChild(h3Logan);
 
-        document.body.appendChild(Logan);
+        // document.body.appendChild(Nate);
 
-        let h3Logan = document.createElement("h3");
-        let h3LoganName = document.createTextNode("Logan");
-        h3Logan.appendChild(h3LoganName);
-        Logan.appendChild(h3Logan);
+        // let h3Nate = document.createElement("h3");
+        // let h3NateName = document.createTextNode("Nate");
+        // h3Nate.appendChild(h3NateName);
+        // Nate.appendChild(h3Nate);
 
-        document.body.appendChild(Nate);
+        // document.body.appendChild(Mike);
 
-        let h3Nate = document.createElement("h3");
-        let h3NateName = document.createTextNode("Nate");
-        h3Nate.appendChild(h3NateName);
-        Nate.appendChild(h3Nate);
+        // let h3Mike = document.createElement("h3");
+        // let h3MikeName = document.createTextNode("Mike");
+        // h3Mike.appendChild(h3MikeName);
+        // Mike.appendChild(h3Mike);
 
-        document.body.appendChild(Mike);
+        // let p = document.createElement("p");
 
-        let h3Mike = document.createElement("h3");
-        let h3MikeName = document.createTextNode("Mike");
-        h3Mike.appendChild(h3MikeName);
-        Mike.appendChild(h3Mike);
+        // Matt.appendChild(p);
+        // Gio.appendChild(p);
+        // Logan.appendChild(p);
+        // Nate.appendChild(p);
+        // Mike.appendChild(p);
 
-        let p = document.createElement("p");
 
-        Matt.appendChild(p);
-        Gio.appendChild(p);
-        Logan.appendChild(p);
-        Nate.appendChild(p);
-        Mike.appendChild(p);
 
-        p.addEventListener(button, function() {
+        //to make a heading element, make the text, put em together, and append
+
 
 
         let friends = ["Matt", "Gio", "Logan", "Nate", "Mike"];
 
         for (let i = 0; i < friends.length; i++) {
 
+            let friendDiv = document.createElement("div");
+            friendDiv.className = "friends";
+
+            let friendHeading = document.createElement("h3");
+            let friendHeadingText = document.createTextNode(friends[i]);
+            friendHeading.appendChild(friendHeadingText);
+
+            document.body.appendChild(friendDiv);
+            friendDiv.appendChild(friendHeading);
+
+
             for (let j = 99; j > 0; j--) {
 
+                let para = document.createElement("p");                
+                friendDiv.appendChild(para);
+                let paraText = document.createTextNode( )
+                
+
+
                 if (j >= 3) {
-                    return (j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out," + [j - 1] + " lines of code in the file.");
+                    (j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out," + [j - 1] + " lines of code in the file.");
                 }
                 else if (j >= 2) {
-                    return (j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out," + [j - 1] + " line of the code in the file.");
+                    (j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out," + [j - 1] + " line of the code in the file.");
                 }
                 else {
-                    return (j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file.");
+                    (j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file.");
+                
                 };
-            }
-        };
 
+
+            }
+        }
     });
-
 });
 
-
-
-
-
-
-
-
-});
